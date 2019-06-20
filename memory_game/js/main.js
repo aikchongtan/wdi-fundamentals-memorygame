e43c1bb6feb20954f0ba9ec4b1adbb807f5c7c05
ghostshiphost <ghostshiphost@gmail.com>
2019-06-20 16:13:25 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -9,9 +9,30 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour);
 */
 
-var cards = ['queen', 'queen', 'king', 'king']
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var cardsInPlay = []
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log("You found a match!");
+    } else {
+            console.log("Sorry, try again.");
+    }
+}
+
+function flipCard(cardId){
+    console.log('User flipped '+cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2){
+        checkForMatch()};
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+/*
 var cardOne = cards[0]
 cardsInPlay.push(cardOne)
 console.log("User flipped " + cardsInPlay[0]);
@@ -25,3 +46,5 @@ if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
 }else {
     alert('Sorry, try again.');
 }
+*/
+
